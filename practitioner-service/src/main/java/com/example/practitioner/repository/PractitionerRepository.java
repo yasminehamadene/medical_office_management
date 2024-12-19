5cac8484b1ceebe3cd1d2c368bd524387194a598
yasminehamadene <120636233+yasminehamadene@users.noreply.github.com>
2024-12-19 12:44:17 +0100

    Delete practitioner-service directory

--- practitioner-service/src/main/java/com/example/practitioner/repository/PractitionerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.practitioner.repository;
-
-import com.example.practitioner.model.Practitioner;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PractitionerRepository extends JpaRepository<Practitioner, Long> {
-} 
