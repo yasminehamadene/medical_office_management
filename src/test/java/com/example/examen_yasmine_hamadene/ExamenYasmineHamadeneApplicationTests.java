bc507b12438354ceade1e637bf5490f8bd3fc33a
yasminehamadene <120636233+yasminehamadene@users.noreply.github.com>
2024-12-19 08:58:29 +0100

    Initial commit for Spring Boot project

---
+++ src/test/java/com/example/examen_yasmine_hamadene/ExamenYasmineHamadeneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.examen_yasmine_hamadene;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExamenYasmineHamadeneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
